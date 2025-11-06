5cfc8c5522ee7391eb7d53c24ec1ccbce95eb78c
jcarrilloh1-estrella <jcarrilloh1@miumg.edu.gt>
2025-11-05 20:55:19 -0600

    Creación del proyecto y clase principal

---
+++ src/controlversiones_danielcarrillo/ControlVersiones_DanielCarrillo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlversiones_danielcarrillo;
+
+/**
+ *
+ * @author DANIEL CARRILLO
+ */
+public class ControlVersiones_DanielCarrillo {
+    public static void main(String[] args) {
+        // Primer Cambio: Creación de la estructura base
+        System.out.println("*** BIENVENIDO AL SISTEMA DE CALCULO BÁSICO ***"); // Mejora
+        int num1 = 10;
+        int num2 = 5;
+        int suma = num1 + num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+
+        // Segundo Cambio: Agregada función para calcular resta
+        int resta = num1 - num2;
+        System.out.println("La resta de " + num1 + " y " + num2 + " es: " + resta);
+
+        // Tercer Cambio: Agregada función para calcular multiplicación
+        int multi = num1 * num2;
+        System.out.println("La multiplicación de " + num1 + " y " + num2 + " es: " + multi); // Nueva función
+    }
+}
